6bdc28ea2ed14a61346b1786bcba8afcdf457cb8
Seiran <seiran.dzhafarov@indusoft.ru>
2024-05-08 10:54:34 +0300

    7/12

---
+++ src/SaveStudentOrder.java
@@ -0,0 +1,13 @@
+public class SaveStudentOrder
+{
+    public static void main(String[] args) // psvm
+    {
+        StudentOrder so = new StudentOrder();
+    }
+
+    static void saveStudentOrder()
+    {
+        long answer = 199;
+        System.out.println("SaveStudentOrder is running");  // sout
+    }
+}
